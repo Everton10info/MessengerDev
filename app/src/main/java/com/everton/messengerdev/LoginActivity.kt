a9382ef27dae2b334ab2816b3813c5f2f27e42b9
Everton Bellmann <everton10info@gmail.com>
2021-06-15 23:45:07 -0300

    Layout initial

--- app/src/main/java/com/everton/messengerdev/LoginActivity.kt
+++ app/src/main/java/com/everton/messengerdev/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.everton.messengerdev
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.act_login)
     }
 }
